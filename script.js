71432ce092d5f717583628bdea79498230f3e8e5
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 13:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
-myGreeting(){
-	return document.getElementById("output").innerHTML = "Hello, world!"
+
+function myGreeting(){
+	 document.getElementById("output").innerHTML = "Hello, world!";
 }
 
 const myTimeout = setTimeout(myGreeting, 1000);
