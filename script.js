ef313c315c99c9223d1e4e734c589faa62310f40
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 12:46:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 nice(){
-	document.getElementById("output").innerHTML = "Hello, world!"
+	return document.getElementById("output").innerHTML = "Hello, world!"
 }
 
-setTimeout(nice();,1000);
+setTimeout(nice(),1000);
